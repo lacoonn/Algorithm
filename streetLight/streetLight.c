8de10c2542c7c2a9af596314ae05a23a0db55127
woochan <teakan7179@gmail.com>
2017-03-31 23:11:17 +0900

    street2

--- streetLight/streetLight.c
+++ streetLight/streetLight.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-#define NUM 6 //°³¼ö
-#define START 5 //½ÃÀÛÀ§Ä¡
+#define NUM 6 //ï¿½ï¿½ï¿½ï¿½
+#define START 5 //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¡
 
-int W[NUM + 1] = { 0, 2, 10, 18, 19, 15, 19 }; //½Ã°£´ç ¼ÒºñÀü·Â
-int D[NUM + 1] = { 0, 3, 11, 12, 13, 15, 17 }; //À§Ä¡
-int EE[NUM + 1][NUM + 1][3]; //¼Ò¸ðÀü·Â
-int DD[NUM + 1][NUM + 1][3]; //ÀÌµ¿°Å¸®
-int PP[NUM + 1][NUM + 1][3]; //ÀÌÀüÀ§Ä¡
+int W[NUM + 1] = { 0, 2, 10, 18, 19, 15, 19 }; //ï¿½Ã°ï¿½ï¿½ï¿½ ï¿½Òºï¿½ï¿½ï¿½ï¿½ï¿½
+int D[NUM + 1] = { 0, 3, 11, 12, 13, 15, 17 }; //ï¿½ï¿½Ä¡
+int EE[NUM + 1][NUM + 1][3]; //ï¿½Ò¸ï¿½ï¿½ï¿½ï¿½ï¿½
+int DD[NUM + 1][NUM + 1][3]; //ï¿½Ìµï¿½ï¿½Å¸ï¿½
+int PP[NUM + 1][NUM + 1][3]; //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¡
+
+int min(int, int);
 
 int main()
 {
@@ -15,7 +17,7 @@ int main()
 
 	EE[START][START][1] = 0;
 	EE[START][START][2] = 0;
-	
+
 	for (i = 1; i <= NUM; i++) {
 		if (i != START) {
 			EE[i][i][1] = 9999;
@@ -25,6 +27,22 @@ int main()
 		DD[i][i][2] = 0;
 		PP[i][i][1] = 0;
 		PP[i][i][2] = 0;
-	
+
 	}
+
+	for (i = 1; i <= NUM; i++) {
+		for (j = i + 1; j <= NUM; j++) {
+			EE[i][j][1] = min(EE[i + 1][j][1] + (DD[i + 1][j][1] + D[i + 1] - D[i]) * W[i],
+												EE[i + 1][j][2] + (DD[i + 1][j][2] + D[j] - D[i]) * W[i]);
+			EE[i][j][2] = min(EE[i][j - 1][2] + (DD[i][j - 1][2] + D[j] - D[j - 1]) * W[j],
+												EE[i][j - 1][1] + (DD[i][j - 1][2] + D[j] - D[i]) * W[j]);
+			
+		}
+	}
+}
+
+int min(a, b) {
+	if (a <= b)
+		return a;
+	return b;
 }
